fddb70f369552e1a88518fc08e4163868003a35d
Giovanni Meroni <giom@dtu.dk>
2022-10-28 13:46:41 +0200

    Drafted algorithm to compute expression from decision tree.

--- DFG2EGSM/export-csv.py
+++ DFG2EGSM/export-csv.py
@@ -2,14 +2,14 @@ import pm4py
 import pandas as pd
 
 dateCols = ['Start time','End time']
-#dataframe = pd.read_csv('../kasterenDataset/log_windowed.csv', sep=',', parse_dates=dateCols)
-dataframe = pd.read_csv('./test2.csv', sep=';', parse_dates=dateCols)
+dataframe = pd.read_csv('../kasterenDataset/log_labeled.csv', sep=',', parse_dates=dateCols)
+#dataframe = pd.read_csv('./test2.csv', sep=';', parse_dates=dateCols)
 dataframe = dataframe.drop_duplicates(subset=['Case_ID', 'Label'], keep='first')
 dataframe = dataframe.rename(columns={'Case_ID': 'case:concept:name', 'Label':'concept:name'})
 
 dataframe = dataframe.set_index(['Start time'])
 # morning routines
-dataframe = dataframe.between_time('09:00', '13:00')
+# dataframe = dataframe.between_time('09:00', '13:00')
 # evening
 # dataframe = dataframe.between_time('18:00', '00:00')
 
@@ -26,7 +26,7 @@ for v in variants:
 # filtering top k variants
 # log = pm4py.filter_variants_top_k(log, 2)
 # filtering variants with at least x% coverage
-# log = pm4py.filter_variants_by_coverage_percentage(log, 0.10)
+log = pm4py.filter_variants_by_coverage_percentage(log, 0.05)
 
 dfg, start_activities, end_activities = pm4py.discover_dfg(log)
 print(dfg)
